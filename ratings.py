d5f22ceff2eb9f012ca79889c93cc2084db6e18b
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2021-07-30 02:54:29 +0000

    implemented function to print ratings in alphabetical order

--- ratings.py
+++ ratings.py
@@ -1,4 +1,16 @@
 """Restaurant rating lister."""
 
 
+
 # put your code here
+def alphabetize_ratings(filename = 'scores.txt'):
+    parsed_ratings = {}
+    file_object = open(filename, "r")
+    for line in file_object:
+        line = line.rstrip()
+        words = line.split(":")
+        parsed_ratings[words[0]] = words[1]
+
+    sorted_restaurants = sorted(parsed_ratings)
+    for restaurant in sorted_restaurants:
+        print(f'{restaurant} is rated at {parsed_ratings[restaurant]}.')
